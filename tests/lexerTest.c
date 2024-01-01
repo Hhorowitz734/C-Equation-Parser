38ff7a9e5ecba87fd7b942c6c25e030f21573c21
Benjamin_Horowitz <hhorowitz734@gmail.com>
2023-12-31 18:48:34 -0600

    Corrected test files passing regardless of actual results

--- tests/lexerTest.c
+++ tests/lexerTest.c
@@ -10,7 +10,7 @@ void basic_parsing_test(void) {
         "(",
         "21",
         "+",
-        "34",
+        "34", 
         ")"
     };
 
@@ -30,6 +30,32 @@ void basic_parsing_test(void) {
 
 }
 
+void test_with_nested_parentheses(void) {
+    char* parsing_input = "((2 + 3) * 5)";
+
+    const char* expected_lexemes[] = {
+        "(",
+        "(",
+        "2",
+        "+",
+        "3",
+        ")",
+        "*",
+        "5", 
+        ")"
+    };
+
+    int position = 0;
+    Token* token;
+
+    for (int i = 0; i < 9; i++) {
+        token = nextToken(parsing_input, &position);
+        if (token == NULL) { break; }
+        CU_ASSERT_STRING_EQUAL(token->lexeme, expected_lexemes[i]);
+    }
+}
+
+
 int main() {
     // Initialize the CUnit test registry
     if (CUE_SUCCESS != CU_initialize_registry())
@@ -42,8 +68,13 @@ int main() {
         return CU_get_error();
     }
 
-    // Add your test to the suite
+    // Add your tests to the suite
     if (NULL == CU_add_test(suite, "Test Basic Parsing", basic_parsing_test)) {
+        CU_cleanup_registry();
+        return CU_get_error();
+    }
+
+    if (NULL == CU_add_test(suite, "Test Nested Parentheses", test_with_nested_parentheses)) {
         CU_cleanup_registry();
         return CU_get_error();
     }
@@ -51,7 +82,13 @@ int main() {
     // Run all tests using the CUnit Basic interface
     CU_basic_set_mode(CU_BRM_VERBOSE);
     CU_basic_run_tests();
+
+    // Capture the number of tests failed
+    int failedTests = CU_get_number_of_failures();
+
+    // Clean up the test registry
     CU_cleanup_registry();
 
-    return CU_get_error();
+    // Return a non-zero value if there were test failures
+    return (failedTests == 0) ? 0 : 1;
 }
