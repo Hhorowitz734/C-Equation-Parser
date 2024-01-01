10b9c4beac613ef08972ec5381598cbc0c5dd1c3
Benjamin_Horowitz <hhorowitz734@gmail.com>
2023-12-31 18:36:13 -0600

    Documented lexer and parser functions

--- include/lexer.h
+++ include/lexer.h
@@ -33,14 +33,70 @@ typedef struct {
     char* lexeme;
 } Token;
 
+/**
+ * @brief Tokenizes a part of the input string, creating Token pointer with lexeme
+ * 
+ * @param input Input string
+ * @param start The start index of the lexeme
+ * @param end The end index of the lexeme (Should be the current position)
+ * 
+ * @return The Token* pointer to an object with the lexeme included
+ * 
+ * @note Remember that the Token* pointer will NOT automatically be given a type
+*/
 Token* tokenizeString(char* input, int start, int *end);
 
+/**
+ * @brief Handles a one-character token such as '(' or ')'
+ * 
+ * @param input Input string
+ * @param start The start index of the lexeme
+ * @param end The end index of the lexeme (Should be start + 1)
+ * @param token_type The type of the atom
+ * 
+ * @return The configured Token* pointer appropriate to the token type and lexeme
+*/
 Token* atom(char* input, int start, int *end, TokenType token_type); //To optimize this, pass a pointer instead
 
+/**
+ * @brief Handles a numeric value (int or float)
+ * 
+ * @param input Input string
+ * @param start The start index of the lexeme
+ * @param end The end index of the lexeme
+ * 
+ * @return The configure Token* pointer for a numeric value
+ * 
+ * @note Note that handling whether the number is a T_INT or T_FLT is handled automatically by this function
+*/
 Token* numeric(char* input, int start, int *end);
 
+/**
+ * @brief Tells us which of the grouping symbols a lexeme is, if any
+ * 
+ * @param grp The symbol to be evaluated
+ * 
+ * @returns The approprate TokenType for the grouping symbol, or T_ERR
+*/
 TokenType GetGroupingType(char grp);
 
+/**
+ * @brief Tells us which of the operators a lexeme is, if any
+ * 
+ * @param opr The symbol to be evaluated
+ * 
+ * @returns The approprate TokenType for the operator symbol, or T_ERR
+*/
+TokenType GetOperatorType(char opr);
+
+/**
+ * @brief Gets the next token in the input string
+ * 
+ * @param input The input string
+ * @param position A reference to the position to start tokenizing from
+ * 
+ * @returns A pointer to the newly created token
+*/
 Token* nextToken(char* input, int *position);
 
 #endif // LEXER_H
--- include/parser.h
+++ include/parser.h
@@ -2,6 +2,7 @@
 #define PARSER_H 
 
 #include "lexer.h"
+#include <stdio.h>
 
 typedef struct {
 
@@ -12,8 +13,39 @@ typedef struct {
     // Left and right children of node
     Token* left;
     Token* right;
-    
+
 } TokenNode;
+
+/**
+ * @brief Creates a parsing tree for an input string
+ * 
+ * @param input The input string to be evaluated
+ * 
+ * @returns The root TokenNode* of the parsing tree
+*/
+TokenNode* getRoot(char* input) {
+
+    int position = 0;
+
+    Token* token = nextToken(input, &position);
+
+    while (token->type != T_ERR) {
+
+        /**
+        switch(token->type) {
+            case T_INT:
+            case T_FLT:
+                // finish implementing
+                // https://stackoverflow.com/questions/4582398/writing-a-simple-equation-parser
+        }
+        */
+
+
+        printf("Token: %s\n", token->lexeme);
+        token = nextToken(input, &position);
+    }
+
+}
 
 
 
